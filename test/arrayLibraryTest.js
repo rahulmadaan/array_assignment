19e83d0c26a47433a3b5faab557d3fef4e075380
Rahul <44020002+rahulmadaan@users.noreply.github.com>
2018-10-17 10:36:27 +0530

    Added new test for reversing arrays
    Added test scenario for reversing arrays. Now, I am merging other
    array tests into one file.

---
+++ test/arrayLibraryTest.js
@@ -0,0 +1,16 @@
+let assert = require('assert');
+let lib = require('../lib/arrayLibrary.js');
+let reverse = lib.reverse;
+
+// empty input
+assert.deepEqual(reverse([]),[]);
+
+// one or two elements
+assert.deepEqual(reverse([1]),[1]);
+assert.deepEqual(reverse([1,2]),[2,1]);
+
+// more than two elements
+assert.deepEqual(reverse([1,2,3,4]),[4,3,2,1]);
+assert.deepEqual(reverse([1,2,3,4,12,34,556,77]),[77,556,34,12,4,3,2,1]);
+assert.deepEqual(reverse([34,55,77,24,73,94,86,336,634,74,373]),[373,74,634,336,86,94,73,24,77,55,34]);
+console.log('Test Passed!!!');
