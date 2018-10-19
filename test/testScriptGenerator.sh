5bf72fe5b17d25130caf676bac6309ff800ecdb0
Rahul <44020002+rahulmadaan@users.noreply.github.com>
2018-10-19 16:51:58 +0530

    Added [] in shellscript and assertion to deepequal
    Added the square brackets '[]' in shell script and
    changes the function equal to deepEqual.

--- test/testScriptGenerator.sh
+++ test/testScriptGenerator.sh
@@ -4,8 +4,7 @@ functionName=$2
 touch $fileName.js
 echo "let assert = require('assert');" >> $fileName.js
 echo "let { $functionName } = require('../lib/arrayLibrary.js');" >> $fileName.js
-echo "" >>$fileName.js
-echo "//" >>$fileName.js
-echo "assert.equal($functionName(),);  " >> $fileName.js
-echo "assert.equal($functionName(),);  " >> $fileName.js
-
+echo "" >> $fileName.js
+echo "//" >> $fileName.js
+echo "assert.deepEqual($functionName([]),);  " >> $fileName.js
+echo "assert.deepEqual($functionName([]),);  " >> $fileName.js
