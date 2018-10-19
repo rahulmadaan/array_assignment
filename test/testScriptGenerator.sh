0c30bfdafdf4cdf6645644aa072c11ce74bb46b4
Rahul <44020002+rahulmadaan@users.noreply.github.com>
2018-10-19 12:36:13 +0530

    Added the funtion name feature to shell script
    Now, the test created with this shell script will have
    the function name and the function name is also
    be copied in the require statement.
    The name of the function is to be given as the second
    parameter of the shell script.
    first parameter in hte shell script is the name of the file without
    any extention. It will automatically put the .js extension.

--- test/testScriptGenerator.sh
+++ test/testScriptGenerator.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 fileName=$1
+functionName=$2
 touch $fileName.js
 echo "let assert = require('assert');" >> $fileName.js
-echo "let lib = require('../lib/arrayLibrary.js');" >> $fileName.js
+echo "let { $functionName } = require('../lib/arrayLibrary.js');" >> $fileName.js
 echo "" >>$fileName.js
-echo "assert.equal((),);  " >> $fileName.js
-echo "assert.equal((),);  " >> $fileName.js
+echo "//" >>$fileName.js
+echo "assert.equal($functionName(),);  " >> $fileName.js
+echo "assert.equal($functionName(),);  " >> $fileName.js
 
